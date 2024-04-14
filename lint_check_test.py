74b9c8cfb1229a839aa9d5d5870ecdbcaf036dea
jslee <dlwhdtjd098@naver.com>
2024-04-14 16:18:26 +0900

    remove: target python file 수정

--- lint_check_test.py
+++ lint_check_test.py
@@ -1,7 +1,7 @@
 from .lint_check_test2 import solution as sl
 
-def solution(a:int, b: int) -> int:
+def solution(a:int,b: int) -> int:
     return a+b
 
 def solution2(a:int, b:int) -> int:
-    return a+b+1
+    return a+b+1+2
