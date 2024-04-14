538cc3de476f011acad0dd281382931776064b79
jslee <dlwhdtjd098@naver.com>
2024-04-14 17:39:36 +0900

    modify: lint 체크 스크립트 수정

--- lint_check_test.py
+++ lint_check_test.py
@@ -1,7 +1,6 @@
-from .lint_check_test2 import solution as sl
+def solution(a: int, b: int) -> int:
+    return a + b + 3 + 4 + 5124
 
-def solution(a:int,b:int) -> int:
-    return a+b+3+4+5124
 
-def solution2(a:int,b:int ) -> int:
-    return a+b+1+123444
+def solution2(a: int, b: int) -> int:
+    return a + b + 1 + 123444
